43fc49e513808741f84c85eb4c18f61f9cf85ef0
Jonah May <119529402+JonahMMay@users.noreply.github.com>
2026-09-22 17:10:04 -0500

    Make speaking animation reach full aperture

--- display/glados_display.py
+++ display/glados_display.py
@@ -37,7 +37,7 @@ SEQUENCES = {
     DisplayState.WAKE: (0, 1, 2, 3, 4, 5, 6, 7, 8),
     DisplayState.LISTENING: (8, 7, 6, 5, 4, 3, 2, 1, 0),
     DisplayState.THINKING: (0, 2, 4, 6, 8, 7, 5, 3, 1),
-    DisplayState.SPEAKING: (1, 2, 3, 4, 5, 6, 7, 8, 7, 6, 5, 4, 3, 2),
+    DisplayState.SPEAKING: (0, 1, 2, 3, 4, 5, 6, 7, 8, 7, 6, 5, 4, 3, 2, 1),
 }
 
 ACTIVE_STATES = {
